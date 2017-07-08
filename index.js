5a440d2948d72760be630c216a563c98ebec0527
Brarian Namonywa <b.namonywa@gmail.com>
2017-07-07 20:08:59 -0500

    layout

--- index.js
+++ index.js
@@ -52,9 +52,24 @@ function showEtsyResults(data) {
     console.log(data.results);
     $.each(data.results, function(key, value) {
         console.log(value.title);
-        //$('.etsy_images').append(`${ value.title } <br>`);s
-        //$("<img/>").attr("src", value.Images[0].url_170x135).appendTo(`.etsy_images`).wrap("<a href='" + value.url + "'></a>");
-        $("<img/>").attr("src", value.Images[0].url_170x135).appendTo(".etsy_images").wrap("<a href='" + value.url + "'></a>");
+        $('.card2').attr("src", value.Images[0].url_170x135);
+        var link = ("<a href=" + value.url + "></a>");
+        var image = "<img src=" + value.Images[0].url_170x135 + ">";
+        $('.etsy_images').append("<div class='card2' > <a href=" + value.url + ">" + image + "</a>" + value.title + "</div>");
+
+        //$("<img/>").attr(.attr("src", value.Images[0].url_170x135).appendTo(`.etsy_images `).wrap("<a href='" + value.url + "'></a>");
+        //$('.img').attr(value.Images[0].url_170x135).appendTo(".etsy_images").wrap("<a href='" + value.url + "'> </a>");
+        //$('.etsy_images').append(` < span class = "image_tags" > $ { title } < /span>`);
+        /*$('.img').hover(
+            function() {
+                console.log("hover works", value.title);
+                $(this).append($(`<span>${value.title} </span>`));
+            },
+            function() {
+                $(this).find("span:last").remove();
+            }
+        );*/
+
     });
 
 }
