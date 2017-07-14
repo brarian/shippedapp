6e538f69e4370370d0fba7e5ead28ecffa13f172
Brarian Namonywa <b.namonywa@gmail.com>
2017-07-13 21:59:06 -0500

    linted pt 1

--- api2.js
+++ api2.js
@@ -1,47 +1,44 @@
-$(document).ready(function() {
-    $('form').submit(function(event) {
-        event.preventDefault();
-        getEtsyResults();
-    });
-});
-
-function getEtsyResults() {
-    console.log('function run');
-    api_key = "oq7bg648maai6ptutm16v8lk";
-    //var shared secrect ="j9z07m17dg"; 
-    terms = $('.etsySearch').val();
-    etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" +
-        terms + "&limit=12&includes=Images:1&api_key=" + api_key;
-
-
-    $.ajax({
-        url: etsyURL,
-        dataType: 'jsonp',
-        success: showEtsyResults
+/* accept $ */
+$(document).ready(function () {
+  $('form').submit(function (event) {
+      event.preventDefault()
+      getEtsyResults()
     })
+})
 
+function getEtsyResults () {
+  console.log('function run')
+  api_key = 'oq7bg648maai6ptutm16v8lk'
+    // var shared secrect ="j9z07m17dg";
+  terms = $('.etsySearch').val()
+  etsyURL = 'https://openapi.etsy.com/v2/listings/active.js?keywords=' +
+        terms + '&limit=12&includes=Images:1&api_key=' + api_key
+
+  $.ajax({
+      url: etsyURL,
+      dataType: 'jsonp',
+      success: showEtsyResults
+    })
 };
 
-function showEtsyResults(data) {
-    console.log(data.results);
-    $.each(data.results, function(key, value) {
-        console.log(value.title);
-        $('.etsy-images').append(`${ value.title } <br>`);
-        $("<img/>").attr("src", value.Images[0].url_170x135).appendTo('.etsy-images').wrap("<a href='" + value.url + "'></a>");
-    });
-
+function showEtsyResults (data) {
+  console.log(data.results)
+  $.each(data.results, function (key, value) {
+      console.log(value.title)
+      $('.etsy-images').append(`${value.title} <br>`)
+      $('<img/>').attr('src', value.Images[0].url_170x135).appendTo('.etsy-images').wrap("<a href='" + value.url + "'></a>")
+    })
 }
 
-showEtsyResults(mockEtsyResults);
+showEtsyResults(mockEtsyResults)
 
-function showEtsyResults(data) {
-    console.log(data.results);
-    $.each(data.results, function(key, value) {
-        console.log(value.title);
-        $('.etsy-images').append(`${ value.title } <br>`);
-        $("<img/>").attr("src", value.Images[0].url_170x135).appendTo('.etsy-images').wrap("<a href='" + value.url + "'></a>");
-    });
-
+function showEtsyResults (data) {
+  console.log(data.results)
+  $.each(data.results, function (key, value) {
+      console.log(value.title)
+      $('.etsy-images').append(`${value.title} <br>`)
+      $('<img/>').attr('src', value.Images[0].url_170x135).appendTo('.etsy-images').wrap("<a href='" + value.url + "'></a>")
+    })
 }
 
-showEtsyResults(mockEtsyResults);
+showEtsyResults(mockEtsyResults)
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+/* global $ */
 $(document).ready(function() {
+    //Hides 'Watch Next' title
     $('.row2').hide();
     $('form').submit(onSubmit);
+})
 
-
-});
-
+//function which calls TasteDive data 
 function getTdResults(userInput) {
-
-
     var tasteTerms = $.ajax({
         type: 'GET',
         url: 'https://tastedive.com/api/similar',
@@ -17,72 +16,59 @@ function getTdResults(userInput) {
             'q': userInput,
             'k': '274917-Stuffyou-HUM4RB26',
             'limit': 6,
-            'type': "shows, movies",
+            'type': 'shows, movies',
             'info': 1
-        },
+        }
 
     })
-    return Promise.resolve(tasteTerms);
+    return Promise.resolve(tasteTerms) //first promise 
 };
-
 
 function onSubmit(event) {
     $('.results').empty();
     $('.etsy_images').empty();
     $('.tagline').remove();
-    $('.row2').fadeIn().delay(15000);
+    $('.row2').fadeIn().delay(25000); //supposed to delay visibility of 'Watch Next' does not work 
     event.preventDefault();
+    //search term for both API calls 
     var userInput = $('.input').val();
-    var promiseRequests = [getTdResults(userInput), getEtsyResults(userInput)];
+    //returns both promises at the same time 
+    var promiseRequests = [getTdResults(userInput), getEtsyResults(userInput)]
     Promise.all(promiseRequests).then(values => {
-        showEtsyResults(values[1]);
-        showTasteDiveData(values[0]);
-    });
-
+        showEtsyResults(values[1])
+        showTasteDiveData(values[0])
+    })
 }
 
-
 function showTasteDiveData(data) {
-    var loop = data.Similar.Results;
-    console.log(data.Similar.Results);
+    var loop = data.Similar.Results
     for (var i = 0; i < loop.length; i++) {
         var videoUrl = loop[i].yUrl;
         var youtubeFrame = (`<iframe width="400" height="350" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>`);
-        console.log(loop[i].Name);
-        var teaser = (loop[i].wTeaser);
-
-        $('.results').append(`<div class="card">${youtubeFrame}<h4>${loop[i].Name}</div>`);
-
+        //adds video and title onto card
+        $('.results').append(`<div class="card">${youtubeFrame}<h4>${loop[i].Name}</div>`)
     }
 }
 
 function getEtsyResults(terms) {
-    var api_key = "oq7bg648maai6ptutm16v8lk";
-    var etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=tv%20" +
-        terms + "&limit=12&includes=Images:1&api_key=" + api_key;
-
+    var apiKey = 'oq7bg648maai6ptutm16v8lk';
+    var etsyURL = 'https://openapi.etsy.com/v2/listings/active.js?keywords=tv%20' +
+        terms + '&limit=12&includes=Images:1&api_key=' + apiKey;
 
     var etsyTerms = $.ajax({
         url: etsyURL,
-        dataType: 'jsonp',
+        dataType: 'jsonp'
+    })
 
-    });
-
-    return Promise.resolve(etsyTerms);
-
+    return Promise.resolve(etsyTerms) //second promise 
 };
 
-
-
 function showEtsyResults(data) {
-    console.log(data.results);
     $.each(data.results, function(key, value) {
         console.log(value.title);
-        $('.card2').attr("src", value.Images[0].url_170x135);
-        var link = ("<a href=" + value.url + "></a>");
-        var image = "<img src=" + value.Images[0].url_170x135 + ">";
-        $('.etsy_images').append("<div class='card2'> <a href=" + value.url + ">" + image + "</a>" + value.title + "</div>");
-
-    });
-
+        //appends title and etsy product image to the card 
+        $('.card2').attr('src', value.Images[0].url_170x135);
+        var image = '<img src=' + value.Images[0].url_170x135 + '>';
+        $('.etsy_images').append("<div class='card2'> <a href=" + value.url + '>' + image + '</a>' + value.title + '</div>');
+    })
 }
